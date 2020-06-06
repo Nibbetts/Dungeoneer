96a89b16e50337430987ef2a01a68bc13e07bd0a
Nibbetts <nathan.paul.tibbetts@gmail.com>
2020-06-06 15:10:08 -0600

    bugfix in find_at

--- dungeon.py
+++ dungeon.py
@@ -456,15 +456,14 @@ class Dungeon:
         found = []
         for p in self.layer_places[z]:
             if p in ignore: continue
-            if (p.x < x) and (p.y < y) and (p.x+p.w-1 > x) and (p.y+p.h-1 > x):
-                if check_borders != False: # True or None
-                    if allow_corners and ((p.x == x) or (p.y == y) or (p.x+p.w-1 == x) or (p.y+p.h-1)) \
-                            or ((p.x == x) ^ (p.y == y) ^ (p.x+p.w-1 == x) ^ (p.y+p.h-1)):
-                        if first_only: return p
-                        else: found.append(p)
-                elif check_borders != True: # False or None
+            if check_borders != False and ((p.x <= x) and (p.y <= y) and (p.x+p.w-1 >= x) and (p.y+p.h-1 >= x)): # True or None
+                if allow_corners and ((p.x == x) or (p.y == y) or (p.x+p.w-1 == x) or (p.y+p.h-1)) \
+                        or ((p.x == x) ^ (p.y == y) ^ (p.x+p.w-1 == x) ^ (p.y+p.h-1)):
                     if first_only: return p
                     else: found.append(p)
+            elif check_borders != True and ((p.x < x) and (p.y < y) and (p.x+p.w-1 > x) and (p.y+p.h-1 > x)): # False or None
+                if first_only: return p
+                else: found.append(p)
         return found
 
     def write(self, place, write_space=False, write_door_in=False, write_loops=False, erase=False, only_overwrite_symbol=None):
